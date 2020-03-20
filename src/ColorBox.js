03801dbe1a62d2f48f61cea97b7187b58520e953
Prakhar-FF13 <prakhar.meerut@gmail.com>
2020-03-20 23:22:38 +0530

    Background overlay

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,11 +2,16 @@ import React, { Component } from "react";
 import "./ColorBox.css";
 import { CopyToClipBoard } from "react-copy-to-clipboard";
 class ColorBox extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { copied: false };
+	}
 	render() {
 		const { name, background } = this.props;
 		return (
 			<CopyToClipBoard text={background}>
 				<div style={{ background: background }} className="ColorBox">
+					<div style={{ background }} className="copy-overlay" />
 					<div className="copy-container">
 						<div className="box-content">
 							<span>{name}</span>
