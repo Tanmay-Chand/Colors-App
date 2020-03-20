b5c815923a9c704537e98be3cd31b93d4f149f02
Prakhar-FF13 <prakhar.meerut@gmail.com>
2020-03-21 00:20:05 +0530

    Transition when copying

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
 import "./ColorBox.css";
-import { CopyToClipBoard } from "react-copy-to-clipboard";
+import CopyToClipBoard from "react-copy-to-clipboard";
 class ColorBox extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { copied: false };
+		this.changeCopyState = this.changeCopyState.bind(this);
+	}
+	changeCopyState() {
+		this.setState({ copied: true }, () => {
+			setTimeout(() => this.setState({ copied: false }), 1500);
+		});
 	}
 	render() {
 		const { name, background } = this.props;
 		return (
-			<CopyToClipBoard text={background}>
+			<CopyToClipBoard text={background} onCopy={this.changeCopyState}>
 				<div style={{ background: background }} className="ColorBox">
-					<div style={{ background }} className="copy-overlay" />
+					<div
+						style={{ background }}
+						className={`copy-overlay ${this.state.copied && "show"}`}
+					/>
 					<div className="copy-container">
 						<div className="box-content">
 							<span>{name}</span>
